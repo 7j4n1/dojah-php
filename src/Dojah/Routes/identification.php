b652722bc37a9e198c6b56f3fd5672b22d059d8d
7j4n1 <ahmaholy@gmail.com>
2022-02-22 19:44:53 +0100

    fix docs

--- src/Dojah/Routes/identification.php
+++ src/Dojah/Routes/identification.php
@@ -2,14 +2,26 @@
 
 	namespace DojahCore\Dojah\Routes;
 
+	/**
+	 * This class contains all KYC/KYB identification endpoints.
+	 *
+	 * @package default
+	 * @author 
+	 **/
+
 	class Identification extends Core {
 
 		
 		/**
-		 * undocumented function
+		 * Validates a bvn
+		 *
+		 * @param string|required bvn 
+		 * @param string|optional first_name 
+		 * @param string|optional last_name 
+		 * @param string|optional dob 
 		 *
 		 * @return Json data from Dojah API
-		 * @author 
+		 * 
 		 **/
 		public function validate_bvn($bvn,$first_name=null,$last_name=null,$dob=null)
 		{
@@ -31,10 +43,14 @@
 			return $response; // Make sure to write utility function to parse this response
 		}
 		/**
-		 * undocumented function
+		 * The Lookup BVN endpoint returns details of a particular BVN
+		 *
+		 * Calls the /api/v1/kyc/bvn/full.
+		 *
+		 * @param string|required bvn 
 		 *
 		 * @return Json data from Dojah API
-		 * @author 
+		 * 
 		 **/
 		public function lookup_bvn_basic($bvn)
 		{
@@ -45,10 +61,14 @@
 			return $this->getClient()->get($this->getBaseUrl . '/api/v1/kyc/bvn/full');	
 		}
 		/**
-		 * undocumented function
+		 * The Lookup BVN endpoint returns details of a particular BVN.
+		 *
+		 * Calls the /api/v1/kyc/bvn/advance.
+		 *
+		 * @param string|required bvn 
 		 *
 		 * @return Json data from Dojah API
-		 * @author 
+		 * 
 		 **/
 		public function lookup_bvn_advance($bvn)
 		{
@@ -59,10 +79,14 @@
 			return $this->getClient()->get($this->getBaseUrl . '/api/v1/kyc/bvn/advance');	
 		}
 		/**
-		 * undocumented function
+		 * The Lookup NUBAN (Nigeria Uniform Bank Account Number) endpoint provides information of users' account numbers.
+		 *
+		 *
+		 * @param string|required account_number 
+		 * @param string|required bank_code 
 		 *
 		 * @return Json data from Dojah API
-		 * @author 
+		 * 
 		 **/
 		public function lookup_nuban($account_number,$bank_code)
 		{
@@ -74,10 +98,13 @@
 			return $this->getClient()->get($this->getBaseUrl . '/api/v1/kyc/nuban');	
 		}
 		/**
-		 * undocumented function
+		 * This endpoint allows developers to fetch customers details.
+		 * using the National Identification Number (NIN) of the customer.
+		 *
+		 * @param string|required nin 
 		 *
 		 * @return Json data from Dojah API
-		 * @author 
+		 * 
 		 **/
 		public function lookup_nin($nin)
 		{
@@ -88,10 +115,12 @@
 			return $this->getClient()->get($this->getBaseUrl . '/api/v1/kyc/nin');	
 		}
 		/**
-		 * undocumented function
+		 * Returns the details of a phone number.
+		 *
+		 * @param string|required phone_number 
 		 *
 		 * @return Json data from Dojah API
-		 * @author 
+		 * 
 		 **/
 		public function lookup_phone_number_basic($phone_number)
 		{
@@ -102,10 +131,12 @@
 			return $this->getClient()->get($this->getBaseUrl . '/api/v1/kyc/phone_number/basic');	
 		}
 		/**
-		 * undocumented function
+		 * Returns the details of a phone number.
+		 *
+		 * @param string|required phone_number 
 		 *
 		 * @return Json data from Dojah API
-		 * @author 
+		 * 
 		 **/
 		public function lookup_phone_number($phone_number)
 		{
@@ -117,10 +148,15 @@
 		}
 
 		/**
-		 * undocumented function
+		 * Lookup vin via date of birth.
+		 *
+		 * @param string|required state 
+		 * @param string|required first_name 
+		 * @param string|required last_name 
+		 * @param string|required dob 
 		 *
 		 * @return Json data from Dojah API
-		 * @author 
+		 * 
 		 **/
 		public function lookup_vin_dob($state,$first_name,$last_name,$dob)
 		{
@@ -135,10 +171,14 @@
 			return $this->getClient()->get($this->getBaseUrl . '/api/v1/kyc/vin');	
 		}
 		/**
-		 * undocumented function
+		 * Lookup vin
+		 *
+		 * @param string|required vin 
+		 * @param string|required state 
+		 * @param string|required last_name 
 		 *
 		 * @return Json data from Dojah API
-		 * @author 
+		 * 
 		 **/
 		public function lookup_vin($vin,$state,$last_name)
 		{
@@ -152,10 +192,13 @@
 			return $this->getClient()->get($this->getBaseUrl . '/api/v1/kyc/vin');	
 		}
 		/**
-		 * undocumented function
+		 * Lookup vin
+		 *
+		 * @param number|required license_number 
+		 * @param yyyy-mm-dd|required dob 
 		 *
 		 * @return Json data from Dojah API
-		 * @author 
+		 * 
 		 **/
 		public function lookup_driver_license($license_number,$dob)
 		{
@@ -167,10 +210,14 @@
 			return $this->getClient()->get($this->getBaseUrl . '/api/v1/kyc/dl');	
 		}
 		/**
-		 * undocumented function
+		 * Fetch CAC information of customers' company/organization.
+		 * CAC is a certificate that shows evidence of the company's existence.
 		 *
+		 * @param string|required rc_number 
+		 * @param string|required company_name 
+		 * 
 		 * @return Json data from Dojah API
-		 * @author 
+		 * 
 		 **/
 		public function lookup_cac($rc_number,$company_name)
 		{
@@ -182,7 +229,9 @@
 			return $this->getClient()->get($this->getBaseUrl . '/api/v1/kyc/cac');	
 		}
 		/**
-		 * undocumented function
+		 * Fetch a person's details using the Tax Identification number.
+		 *
+		 * @param string|required tin
 		 *
 		 * @return Json data from Dojah API
 		 * @author 
